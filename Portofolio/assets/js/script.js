14f9bbac73de9d5f60e160f43448d58e3c783dc5
Hhhhiiii05 <adit34072@gmail.com>
2024-05-09 20:07:31 +0700

    Upload Porto ver 1

---
+++ Portofolio/assets/js/script.js
@@ -0,0 +1,32 @@
+AOS.init()
+
+/* Navbar  */
+const show = document.getElementById('show-nav')
+const hide = document.getElementById('hide-nav')
+const hide2 = document.getElementById('hide-nav2')
+const navbar = document.getElementById('navbar')
+
+show.addEventListener('click', () => {
+    navbar.classList.add('show')
+    navbar.classList.remove('hide')    
+})
+
+hide.addEventListener('click', () => {
+    navbar.classList.add('hide') 
+    navbar.classList.remove('add')
+})
+
+hide2.addEventListener('click', () => {
+    navbar.classList.add('hide') 
+    navbar.classList.remove('add')
+})
+
+/* More info */
+
+const moreInfoBtn = document.getElementById('more-info')
+const moreTab = document.getElementById('more-tab')
+const arrow = document.getElementById('arrow')
+
+moreInfoBtn.addEventListener('click', () => {
+    moreTab.classList.toggle('start')
+})
