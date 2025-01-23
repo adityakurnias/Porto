8ada2b52604749b835d61cdac821206bdf93a1fa
adityakurnias <adityakurniasaputra903@gmail.com>
2025-01-23 21:07:12 +0700

    Format code

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,11 +1,9 @@
-function sendMail(){
+function sendMail() {
   let parms = {
-    from_name : document.getElementById("name").value,
-    email : document.getElementById("email").value,
-    message : document.getElementById("message").value,
-  }
-  
-  emailjs.send('', '', parms).then(
-  console.log("pesan terkirim")
-  )
+    from_name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs.send("", "", parms).then(console.log("pesan terkirim"));
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,59 @@
 //AOS
 AOS.init({
-    easing: 'ease-out-back',
-    duration: 1000
+  easing: "ease-out-back",
+  duration: 1000,
 });
 
 /* Navbar  */
-const navbar = document.getElementById('navbar');
+const navbar = document.getElementById("navbar");
 
 function toggleNavbar(action) {
-    navbar.classList.add(action === 'show' ? 'show' : 'hide');
-    navbar.classList.remove(action === 'show' ? 'hide' : 'show');
+  navbar.classList.add(action === "show" ? "show" : "hide");
+  navbar.classList.remove(action === "show" ? "hide" : "show");
 }
 
-document.getElementById('show-nav').addEventListener('click', () => toggleNavbar('show'));
-document.getElementById('hide-nav').addEventListener('click', () => toggleNavbar('hide'));
+document
+  .getElementById("show-nav")
+  .addEventListener("click", () => toggleNavbar("show"));
+document
+  .getElementById("hide-nav")
+  .addEventListener("click", () => toggleNavbar("hide"));
 
-document.querySelectorAll('.navbar a').forEach(link => {
-    link.addEventListener('click', () => toggleNavbar('hide'));
+document.querySelectorAll(".navbar a").forEach((link) => {
+  link.addEventListener("click", () => toggleNavbar("hide"));
 });
 
 /* More info */
-const moreInfoBtn = document.getElementById('more-info')
-const moreTab = document.getElementById('more-tab')
+const moreInfoBtn = document.getElementById("more-info");
+const moreTab = document.getElementById("more-tab");
 
-moreInfoBtn.addEventListener('click', () => {
-    moreTab.classList.toggle('start')
-})
+moreInfoBtn.addEventListener("click", () => {
+  moreTab.classList.toggle("start");
+});
 
-const sections = document.querySelectorAll('section')
-const navLink = document.getElementsByClassName('nlink')
+const sections = document.querySelectorAll("section");
+const navLink = document.getElementsByClassName("nlink");
 
-window.addEventListener('scroll', () => {
-    let current = ''
+window.addEventListener("scroll", () => {
+  let current = "";
 
-    sections.forEach(section => {
-        const sectTop = section.offsetTop
-        const sectHeight = section.clientHeight
+  sections.forEach((section) => {
+    const sectTop = section.offsetTop;
+    const sectHeight = section.clientHeight;
 
-        if ( window.scrollY >= sectTop - sectHeight / 3 ) {
-            current = section.getAttribute('id')
-        }
-    })
+    if (window.scrollY >= sectTop - sectHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
 
-    Array.from(navLink).forEach(link => {
-        link.classList.remove('active') 
+  Array.from(navLink).forEach((link) => {
+    link.classList.remove("active");
 
-        if ( link.getAttribute('href') === `#${current}` ) {
-            link.classList.add('active')
-        }
-    })
-})
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   const lenis = new Lenis();
